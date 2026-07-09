79fa6566410244075edfa4e6ef60aca77906be83
djaffic <fatei-25@yandex.ru>
2026-07-09 22:37:45 +0300

    Add expenses script

---
+++ 4-expenses/list.py
@@ -0,0 +1,8 @@
+expenses = [100, 200, 300, 400, 500, 600, 700]
+
+print(sum(expenses))
+# print(len(expenses))
+print(sum(expenses) / len(expenses))
+print(max(expenses))
+print(min(expenses))
+print((min(expenses), max(expenses), sum(expenses)))
