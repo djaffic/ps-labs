5d581a2dd0adf038db0a80720dbbc23436b8804a
djaffic <fatei-25@yandex.ru>
2026-07-15 23:12:39 +0300

    Add some fix

--- 4-expenses/price.py
+++ 4-expenses/price.py
@@ -2,10 +2,18 @@ sum = input("Введите сумму: ").strip()
 
 if len(sum.split()) == 2:
     integer = sum.split()[0]
-    if integer.isdigit():
+    rub = sum.split()[1]
+    if integer.isdigit() and rub == "руб":
         print(f"{int(integer):.2f} ₽")
 elif len(sum.split()) == 4:
     integer = sum.split()[0]
+    rub = sum.split()[1]
     fractional = sum.split()[2]
-    if integer.isdigit() and fractional.isdigit():
-        print(f"{integer}.{fractional} ₽")
+    kop = sum.split()[3]
+    if integer.isdigit() and fractional.isdigit() and rub == "руб" and kop == "коп":
+        if len(fractional) == 1:
+            print(f"{integer}.0{fractional} ₽")
+        elif len(fractional) == 2:
+            print(f"{integer}.{fractional} ₽")
+else:
+    print("Некорректный формат суммы")
